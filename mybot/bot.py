a8bbc0bd4ddd7e580589e2740fb9cec2cb9b813e
Alexey <fav1290@gmail.com>
2023-05-14 14:36:21 +0300

    My homework is here in order not to be eaten by my dog

--- mybot/bot.py
+++ mybot/bot.py
@@ -2,6 +2,12 @@ import logging
 import settings
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 logging.basicConfig(filename='bot.log', level=logging.INFO)
+version = '0.0000000000000000002'
+
+
+def my_version(update, context):
+    print('Вызван /verson')
+    update.message.reply_text(f'Привет! Это версия бота №{version}')    
 
 
 def talk_to_me(update, context):
@@ -19,11 +25,12 @@ def main():
     mybot = Updater(settings.API_KEY, use_context=True)
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("version", my_version))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
-    logging.info("Бот стартовал")
+    logging.info('Bot has started')
     mybot.start_polling()
     mybot.idle()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
